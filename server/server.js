4a7a05c27119ef9ebd36c442cd1fca64cc0e18b8
MrStark135 <g3vukovic15@gmail.com>
2025-11-11 17:47:53 +0100

    Add cors fix

--- server/server.js
+++ server/server.js
@@ -33,6 +33,7 @@ app.get('/messages', (req, res) => {
 			content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 		}
 	];
+	res.send('Access-Control-Allow-Origin', 'https://'+process.env.CLIENT_HOSTNAME);
 	res.send(messages);
 });
 app.get('/env', (req, res) => {
